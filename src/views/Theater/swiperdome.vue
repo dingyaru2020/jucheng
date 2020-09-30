372f772aead5d7537dc010fe5819b2bf327f6f49
丫头 <1603962865@qq.com>
2020-09-30 15:52:45 +0800

    swiper插件

---
+++ src/views/Theater/swiperdome.vue
@@ -0,0 +1,26 @@
+ <template>
+    <swiper ref="mySwiper" :options="swiperOptions">
+        <swiper-slide>Slide 1</swiper-slide>
+        <swiper-slide>Slide 2</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+        <swiper-slide>Slide 4</swiper-slide>
+        <swiper-slide>Slide 5</swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+</template>
+
+
+<script>
+export default {
+    swiperOption: {
+        pagination: {
+            el: '.swiper-pagination'
+        },
+        // Some Swiper option/callback...
+    },
+}
+</script>
+
+<style>
+
+</style>
