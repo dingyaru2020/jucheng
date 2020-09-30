67c9d15e2647316847c8416174d2f2c2f4c3c318
丫头 <1603962865@qq.com>
2020-09-30 09:10:29 +0800

    新建topbar

---
+++ src/components/TopBar/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="theater-head">
+        <div class="head_nav">
+            <h3>{{title}}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+
+  
+
+export default {
+    name:"TopBar",
+    props:[title],
+    data () {
+        return {
+        
+        }
+    },
+    mounted () {
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .theater-head {
+        .head_nav {
+            width: 100%;
+            height: 88px;
+            background: #222
+
+        }
+    };
+    
+</style>>
--- src/views/Theater/index.vue
+++ src/views/Theater/index.vue
@@ -1,13 +1,49 @@
 <template>
-  <div>剧院</div>
+    <div class="theater-head">
+        <TopBar :title="剧院"></TopBar>
+    </div>
 </template>
 
 <script>
+
+  import TopBar from "../../components/TopBar"
+
 export default {
-  name: 'Theater',
+    
+    name:"Theater",
+    data () {
+        return {
+            page:1,
+            version:"6.1.1",
+            referer:2,
+            theatre_list:[]
+        }
+    },
+    async mounted () {
+        const res =await this.$axios.get(`https://api.juooo.com/theatre/index/getTheatreList?page=${this.page}&version=${this.version}&referer${this.referer}`,)  
+        // console.log(res)
+        this.theatre_list = res.data.theatre_list
+    },
+    computed: {
+        title(){
+            // return this.$router
+        }
+    },     // console.log(this.$route.query)
+    components: {
+        TopBar
+    }
+  
 }
 </script>
 
 <style lang="less" scoped>
+    .theater-head {
+        .head_nav {
+            width: 100%;
+            height: 88px;
+            background: #222
 
-</style>
+        }
+    };
+    
+</style>>
