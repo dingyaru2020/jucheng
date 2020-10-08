34e5d1722f6c8931009d0ae16934c298f58aa534
shihai.hou <572261346@qq.com>
2020-10-08 12:10:50 +0800

    Hou

--- src/components/card/index.vue
+++ src/components/card/index.vue
@@ -1,17 +1,35 @@
 <template>
     <div>
-        <div class="cardTitle">
-            <!-- cate_card: Array(4)
-                city_title: ""
-                once_card: Array(6)
-                store_card: Array(1)
-                store_title: "全国通用，购卡充值送100元" -->
-            <span v-if=onceCard?true:false>{{onceCard.store_card[0].name}}</span>
-            <!-- <span>{{onceCard.store_title}}</span> -->
-            <span >{{onceCard.store_title}}</span>
-
-        </div>
-        <div class="cardBody"></div>
+        <div class="entry-block margin-top">
+                
+                <div class="entry-block__title">
+                    {{once_card.store_card[0].card_type_name}}
+                <span class="entry-block__title__desc">{{once_card.store_title}}</span>
+                </div>
+                 <div class="entry-card-list">
+                     <div class="card-list__item">
+                         <div class="orange-plus">
+                             <div class="card-cell">
+                             <div class="image card-cell__bg-img">
+                                 <img :src="once_card.store_card[0].card_image" class="ju-image ju-image--fill">
+                             </div>
+                             <div class="card-cell__desc">
+                                 <div class="card-cell__desc__top">
+                                {{once_card.store_card[0].name}}
+                                </div>
+                                <div class="card-cell__desc__middle">有效期： 长期有效</div>
+                                <div class="card-cell__desc__bottom">
+                                    <div class="orange-plus__price">
+                                        <span class="orange-plus__price__num">¥{{once_card.store_card[0].card_price}}</span>
+                                         <span class="orange-plus__price__give">赠VIP+</span>
+                                    </div>
+                                </div>
+                             </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+            </div>
     </div>
 </template>
 
