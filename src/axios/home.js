5ac0aaf85c018768d66e770637613c1c2e683cdf
unknown <18862453498@163.com>
2020-10-09 09:15:38 +0800

    ...

--- src/axios/home.js
+++ src/axios/home.js
@@ -1,4 +1,29 @@
-import axios from "./axios"
-export default{
-    
-}
+import axios from "axios";
+import qs from "qs"; //用于登录界面传递参数formdata形式
+
+const instance = axios.create({
+  baseURL: "",
+  timeout: 10000,
+  //？？？？？
+  transformRequest: [
+    function(data) {
+      return qs.stringify(data);
+    }
+  ]
+});
+instance.interceptors.request.use(config => {
+  const authorization = localStorage.getItem("token");
+  if (authorization) {
+    config.headers["AUTHORIZATION"] = authorization;
+  }
+  return config;
+});
+instance.interceptors.response.use(
+  response => {
+    return response.data;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+export default instance;
